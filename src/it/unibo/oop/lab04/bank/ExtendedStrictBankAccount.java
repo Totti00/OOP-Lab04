f66ab7fc82e5d428395e2a6bb2c0d0019f038ece
Giacomo <giacomo.totaro2@studio.unibo.it>
2020-11-14 15:02:21 +0100

    Modificato

--- src/it/unibo/oop/lab04/bank/ExtendedStrictBankAccount.java
+++ src/it/unibo/oop/lab04/bank/ExtendedStrictBankAccount.java
@@ -2,29 +2,27 @@ package it.unibo.oop.lab04.bank;
 
 public class ExtendedStrictBankAccount extends SimpleBankAccount {
 
-    private static final double TRANSACTION_FEE = 0.1;
+	private static final double TRANSACTION_FEE = 0.1;
 	
 	public ExtendedStrictBankAccount(int usrID, double balance) {
 		super(usrID, balance);
 	}
-	
-    private boolean isWithdrawAllowed(final double amount) {
-        return this.getBalance() > amount;
-    }
 
+    private boolean isWithdrawAllowed(final double amount) {
+        return getBalance() > amount;
+    }
+	
     public void computeManagementFees(final int usrID) {
         final double feeAmount = MANAGEMENT_FEE + (this.getNTransactions() * TRANSACTION_FEE);
         if (checkUser(usrID) && isWithdrawAllowed(feeAmount)) {
-            this.setBalance(this.getBalance()-feeAmount);
+        	this.setBalance(this.getBalance()-feeAmount);
             this.resetTransactions();
         }
     }
     
     public void withdraw(final int usrID, final double amount) {
         if (isWithdrawAllowed(amount)) {
-        	
-            super.withdraw(usrID, amount);
+            super.withdraw(usrID, -amount);
         }
     }
-
 }
